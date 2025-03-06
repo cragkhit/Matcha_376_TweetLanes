8df08a952f5b1e8ea59dc7d2f20cbeec0a4e4a52
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2025-03-06 10:47:51 +0700

    Update ImageViewActivity.java
    Added `try` and `finally` to make sure that the input stream and output stream are closed properly.

--- android/app/src/main/java/com/tweetlanes/android/core/view/ImageViewActivity.java
+++ android/app/src/main/java/com/tweetlanes/android/core/view/ImageViewActivity.java
@@ -183,16 +183,21 @@ public class ImageViewActivity extends FragmentActivity {
 
     public void copy(File src, File dst) throws IOException {
         InputStream in = new FileInputStream(src);
-        OutputStream out = new FileOutputStream(dst);
-
-        // Transfer bytes from in to out
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0) {
-            out.write(buf, 0, len);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
         }
-        in.close();
-        out.close();
     }
 
     /*
